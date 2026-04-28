efb7ddcdfde818a0a281af42379ad6028e31e0f8
twoyosea24c-debug <twoyosea24c@gmail.com>
2026-04-28 15:16:53 +0900

    Supabase確認ページを管理者専用に制限

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
         <Route
           path="settings/supabase"
           element={
-            <RequireAdmin>
+            <RequireAdmin redirectOnDeniedTo="/">
               <SupabaseSettingsPage />
             </RequireAdmin>
           }
@@ -49,7 +49,7 @@ function App() {
         <Route
           path="settings/supabase-shops"
           element={
-            <RequireAdmin>
+            <RequireAdmin redirectOnDeniedTo="/">
               <SupabaseShopsPage />
             </RequireAdmin>
           }
